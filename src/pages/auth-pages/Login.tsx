c2e69cba9b7532feb4c7fc750af1a383d2488aa9
Armen Babayan <armenbabayan1984@gmail.com>
2026-08-11 00:17:31 +0400

    login page style

--- src/pages/auth-pages/Login.tsx
+++ src/pages/auth-pages/Login.tsx
@@ -3,9 +3,17 @@ import Button from '@mui/material/Button';
 import loginStyles from "../../shared/styles/pages/login.module.scss"; 
 import { useState } from 'react';
 import TextField from '@mui/material/TextField';
+import { Link, useNavigate } from 'react-router-dom';
 
 export function Login() {
     const [showLogin, setShowLogin] = useState<boolean>(false);
+
+    const navigate = useNavigate();
+
+    const onLogin = () => {
+        // some api call logic
+        navigate("/home");
+    }
     
     return (
         // <>
@@ -28,8 +36,11 @@ export function Login() {
                     <div className={loginStyles.login_form}>
                         <TextField size='small' sx={{ width: '90%'}} id="outlined-basic" label="Login" variant="outlined" />
                         <TextField size='small' sx={{ width: '90%'}} id="outlined-password-input" label="Password" type="password" autoComplete="current-password" />
-                        <Button variant="contained" sx={{ width: '90%'}}>Login</Button>
-                        <span className={loginStyles.register}>register</span>
+                        <Button variant="outlined" sx={{ width: '90%'}} onClick={onLogin}>Login</Button>
+                        <div className={loginStyles.register_container}>
+                            <span className={loginStyles.dont_have_account}>Dont have account?</span>
+                            <Link to="/register" className={loginStyles.register}>Register</Link>
+                        </div>
                     </div>
                 ) 
             }
