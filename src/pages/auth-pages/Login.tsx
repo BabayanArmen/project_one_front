318b09e0d0ca20eeef9e2ebcbd7383393c75a663
Armen Babayan <armenbabayan1984@gmail.com>
2026-08-11 17:09:20 +0400

    auth methods

--- src/pages/auth-pages/Login.tsx
+++ src/pages/auth-pages/Login.tsx
@@ -1,18 +1,40 @@
 // import { decrement, increment, reset } from "../../zustand-store/useCounterStore";
 import Button from '@mui/material/Button';
-import loginStyles from "../../shared/styles/pages/login.module.scss"; 
-import { useState } from 'react';
+import styles from "../../shared/styles/pages/login.module.scss"; 
 import TextField from '@mui/material/TextField';
 import { Link, useNavigate } from 'react-router-dom';
+import z from 'zod';
+import { useForm, type SubmitHandler } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
 
 export function Login() {
-    const [showLogin, setShowLogin] = useState<boolean>(false);
-
     const navigate = useNavigate();
 
-    const onLogin = () => {
-        // some api call logic
-        navigate("/home");
+    const schema = z.object({
+        username: z.string().min(1, "Login is required"),
+        password: z.string().min(1, "Password is required")
+    })
+
+    type FormFields = z.infer<typeof schema>;
+
+    const { register, handleSubmit, formState: { errors, isSubmitting } } = useForm<FormFields>({
+        defaultValues: {
+            username: '',
+            password: ''
+        },
+        resolver: zodResolver(schema)
+    })
+
+    const onSubmit: SubmitHandler<FormFields> = async (data) => {
+        const res = await new Promise((resolve) => {
+            setTimeout(() => {
+                return resolve(data);
+            }, 3000)
+        })
+        if (res) {
+            console.log(res);
+            navigate("/home");
+        }
     }
     
     return (
@@ -23,27 +45,40 @@ export function Login() {
         //     <span>Login</span>
         // </>
 
-        <div className={loginStyles.login_page_wrapper}>
-            <div className={loginStyles.login_content}>
-                <div className={loginStyles.login_title}>
-                    Project ONE
-                    {!showLogin && <Button variant="contained" size="small" onClick={() => setShowLogin(true)}>Enter</Button>}
-                </div>
+        <div className={styles.login_wrapper}>
+            <div className={styles.login_content}>
+                <div className={styles.login_title}>Project ONE</div>
             </div>
-            {
-                showLogin && 
-                (
-                    <div className={loginStyles.login_form}>
-                        <TextField size='small' sx={{ width: '90%'}} id="outlined-basic" label="Login" variant="outlined" />
-                        <TextField size='small' sx={{ width: '90%'}} id="outlined-password-input" label="Password" type="password" autoComplete="current-password" />
-                        <Button variant="outlined" sx={{ width: '90%'}} onClick={onLogin}>Login</Button>
-                        <div className={loginStyles.register_container}>
-                            <span className={loginStyles.dont_have_account}>Dont have account?</span>
-                            <Link to="/register" className={loginStyles.register}>Register</Link>
-                        </div>
-                    </div>
-                ) 
-            }
+            <form className={styles.login_form} onSubmit={handleSubmit(onSubmit)}>
+                <TextField
+                    {...register('username')} 
+                    size='small' 
+                    sx={{ width: '90%'}} 
+                    id="outlined-basic" 
+                    label="Login" 
+                    variant="outlined"
+                    error={!!errors.username}
+                    helperText={errors.username?.message}
+                />
+                <TextField
+                    {...register('password')}
+                    size='small' 
+                    sx={{ width: '90%'}} 
+                    id="outlined-password-input" 
+                    label="Password" 
+                    type="password" 
+                    autoComplete="current-password"
+                    error={!!errors.password}
+                    helperText={errors.password?.message}
+                />
+                <Button type='submit' variant="outlined" sx={{ width: '90%'}} disabled={isSubmitting}>
+                    {isSubmitting ? 'Loading' : 'Enter'}
+                </Button>
+                <div className={styles.register_container}>
+                    <span className={styles.dont_have_account}>Dont have account?</span>
+                    <Link to="/register" className={`${styles.register} ${isSubmitting ? styles.disable : ''}`}>Register</Link>
+                </div>
+            </form>
         </div>
     )
 }
--- src/pages/auth-pages/Register.tsx
+++ src/pages/auth-pages/Register.tsx
@@ -1,5 +1,76 @@
+import { useNavigate } from "react-router-dom";
+import styles from "../../shared/styles/pages/register.module.scss";
+import Button from '@mui/material/Button';
+import z from "zod";
+import { useForm, type SubmitHandler } from "react-hook-form";
+import TextField from '@mui/material/TextField';
+import FormControlLabel from '@mui/material/FormControlLabel';
+import Checkbox from '@mui/material/Checkbox';
+import Box from '@mui/material/Box';
+import { zodResolver } from "@hookform/resolvers/zod";
+
 export function Register() {
+    const navigate = useNavigate();
+
+    const schema = z.object({
+        username: z.string().min(1, "User Name is required"),
+        email: z.email("Enter valid email").min(1, "Email is required"),
+        password: z.string().min(1, "Password is required"),
+        isOrganization: z.boolean().optional(),
+        organizationName: z.string().optional()
+    })
+    .refine(
+       (data) => !(data.isOrganization && (!data.organizationName || data.organizationName.trim() == "" || data.organizationName == undefined)),
+       {
+        message: "Organization name is required",
+        path: ["organizationName"]
+       }  
+    )
+
+    type FormFields = z.infer<typeof schema>;
+
+    const { register, handleSubmit, formState: { errors, isSubmitting }, watch, } = useForm<FormFields>({
+        defaultValues: {
+            username: '',
+            email: '',
+            password: '',
+            isOrganization: false,
+            organizationName: ''
+        },
+        resolver: zodResolver(schema)
+    })
+
+    const onSubmit: SubmitHandler<FormFields> = async (data) => {
+        const res = await new Promise((resolve) => {
+            setTimeout(() => {
+                resolve(data)
+            }, 3000)
+        })
+        if (res) {
+            console.log(res);
+            navigate("/")
+        }
+    }
+
     return (
-        <span>Register</span>
+        <div className={styles.wrapper}>
+            <div className={styles.content}>
+                <Button variant="outlined" sx={{color: 'white', ml: 2, mt: 1}} onClick={() => navigate("/")} >Back</Button>
+            </div>
+            <form className={styles.form} onSubmit={handleSubmit(onSubmit)}>
+                <TextField {...register("username")} error={!!errors.username} helperText={errors.username?.message} id="outlined-basic" label="Username" variant="outlined" size="small" sx={{width: '90%'}} />
+                <TextField {...register("email")} error={!!errors.email} helperText={errors.email?.message} id="outlined-basic" label="Email" variant="outlined" size="small" sx={{width: '90%'}}/>
+                <TextField {...register("password")} error={!!errors.password} helperText={errors.password?.message} id="outlined-basic" label="Password" variant="outlined" size="small" sx={{width: '90%'}}/>
+                <Box sx={{width: '90%', textAlign: 'start'}}>
+                    <FormControlLabel control={<Checkbox defaultChecked={false} {...register("isOrganization")} />} label="Is Organization" />
+                </Box>
+                {watch('isOrganization') && (
+                    <TextField {...register("organizationName")} error={!!errors.organizationName} helperText={errors.organizationName?.message} id="outlined-basic" label="Organization Name" variant="outlined" size="small" sx={{width: '90%'}}/>
+                )}
+                <Button type="submit" variant="outlined" sx={{width: '90%'}} disabled={isSubmitting}>
+                    {isSubmitting ? "Loading" : "Enter"}
+                </Button>
+            </form>
+        </div>
     )
 }
