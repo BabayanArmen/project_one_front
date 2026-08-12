e603e9ac63d754e7cc2dd9d3ee3252ccfc9c111e
Armen Babayan <armenbabayan1984@gmail.com>
2026-08-12 18:09:36 +0400

    asdas

--- src/pages/main-pages/Home.tsx
+++ src/pages/main-pages/Home.tsx
@@ -1,5 +1,26 @@
+import { useEffect, useState } from "react";
+
 export function Home() {
+    const [value, setValue] = useState<number>(0);
+
+
+
+    useEffect(() => {
+    }, [value]);
+
+    const inc = () => {
+        setValue(value => value + 1);
+    }
+
     return (
-        <div>Home page</div>
+        <>
+            <div>Home page</div>
+
+            <button onClick={inc}>inc</button>
+
+            <span>{value}</span>
+
+            {/* <span>{counter.current}</span> */}
+        </>
     )
 }
